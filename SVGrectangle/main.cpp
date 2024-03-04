6d4ea70c9e0690facdc0073e44cf318550580588
rloftus3 <162064076+rloftus3@users.noreply.github.com>
2024-03-04 03:12:43 +0000

    redtangle

--- SVGrectangle/main.cpp
+++ SVGrectangle/main.cpp
@@ -8,12 +8,14 @@ int main()
 {
    cout << "Content-type: text/html" << endl << endl;
    
-   // create the objects using the constructor and/or setters here
+   SVGrectangle redtangle("0", "0", "300", "100", "255", "0", "0");
+   
+
 
    // start the svg canvas
    cout << "<svg width='1000' height='400'>" << endl;
 
-   // print the three rectangles you created here
+   redtangle.printHTML();
 
    // end the svg canvas
    cout << "</svg>" << endl;
