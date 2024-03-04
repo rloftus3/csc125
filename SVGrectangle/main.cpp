4b253212fb19b9c80e80809effc6921542cbd8b2
rloftus3 <162064076+rloftus3@users.noreply.github.com>
2024-03-04 02:33:37 +0000

    test

---
+++ SVGrectangle/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "SVGrectangle.h"
+
+using namespace std;
+
+int main()
+{
+   cout << "Content-type: text/html" << endl << endl;
+   
+   // create the objects using the constructor and/or setters here
+
+   // start the svg canvas
+   cout << "<svg width='1000' height='400'>" << endl;
+
+   // print the three rectangles you created here
+
+   // end the svg canvas
+   cout << "</svg>" << endl;
+
+   return 0;
+}
+   
