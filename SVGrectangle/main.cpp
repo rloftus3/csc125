4170dd51b8d4ce8354f468864807bf75582eca31
rloftus3 <162064076+rloftus3@users.noreply.github.com>
2024-03-04 03:30:12 +0000

    rect2

--- SVGrectangle/main.cpp
+++ SVGrectangle/main.cpp
@@ -8,9 +8,9 @@ int main()
 {
    cout << "Content-type: text/html" << endl << endl;
    
-   SVGrectangle redtangle("200", "150", "300", "100", "255", "0", "0");
-   SVGrectangle greentangle("800", "100", "300", "100", "0", "255", "0");
-   SVGrectangle bluetangle("0", "0", "300", "100", "0", "0", "255");
+   SVGrectangle redtangle("350", "200", "300", "100", "255", "0", "0");
+   SVGrectangle greentangle("0", "300", "300", "100", "0", "255", "0");
+   SVGrectangle bluetangle("310", "300", "300", "100", "0", "0", "255");
 
    // start the svg canvas
    cout << "<svg width='1000' height='400'>" << endl;
