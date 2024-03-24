497dc4532be6f5a6203d08e703faa7dae0c9ee63
rloftus3 <162064076+rloftus3@users.noreply.github.com>
2024-03-24 01:42:53 +0000

    tets

---
+++ parentheses/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "htmlform.h"
+#include <stack>
+
+using namespace std;
+
+int main() {
+
+  stack<char> s;
+
+  string inputText;
+  HTMLform textarea;
+
+  /// complete the http header
+  cout << "Content-type: text/html\n\n";
+
+  /// build a form object
+
+  textarea.getPostData(); /// reads the form and inits the object
+
+  /// Show the debugging (optional)
+  //textarea.debug();
+
+  /// the output ('name' is in the html form that started this)
+  inputText = textarea.getField("text");
+  cout << inputText;
+
+  return 0;
+}
