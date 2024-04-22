dafb69e0c13b36088d370f4dda768f1b556347b8
rloftus3 <162064076+rloftus3@users.noreply.github.com>
2024-04-22 03:33:36 +0000

    parse

--- tree/main.cpp
+++ tree/main.cpp
@@ -10,6 +10,7 @@
 #include "node.h"
 
 void printProgram(string);   /// print the program in HTML
+void parseProgram(string);
 
 using namespace std;
 
@@ -32,9 +33,10 @@ int main()
   string program = steps.getField("program");
   
   printProgram(program);
+  cout << "<hr/>";
   
   //The next line is for you to complete
-  //parseProgram(program);
+  parseProgram(program);
 
   return 0;
 }
@@ -57,9 +59,62 @@ void printProgram(string program)
       
       
       /// here's where you need to process 'line'
+
+
       /// this routine just prints it.
       cout << ++count << ": [" << line << "] len=" << line.size() 
            << "<br />" << endl;
     }
+
+}
+
+void parseProgram(string program)
+{
+  /// a single line
+  string line;
+  /// for counting the lines
+  int count = 0;
+  /// where the newline is (for peeling off a line)
+  int pos;
+  int sep;
+  string token;
+  string loc;
+  string cmd;
+  int locint;
+
+  cout << "<table>";
+
+  while ( string::npos != (pos = program.find("\n") ) )
+    {
+      line = program.substr(0, pos);    /// before the newline
+      program = program.substr(pos+1);  /// after the newline
+      /// there might newline characters at the end of line
+      if (line[pos] < '0') line.erase(pos-1);
+      
+      
+      /// here's where you need to process 'line'
+      sep = line.find(" ");
+      cmd = line.substr(0, sep);
+
+      cout << "<tr>";
+      cout << "<td>" << cmd << "</td>";
+
+      if(cmd == "INSERT") {
+        line = line.substr(sep+1);
+        sep = line.find(",");
+        loc = line.substr(0, sep);
+        locint = stoi(loc);
+        token = line.substr(sep+1);
+        cout << "<td>" << locint << "</td>";
+        cout << "<td>" << token << "</td>";
+      }
+      cout << "</tr>";
+      
+      
+      /// this routine just prints it.
+      //cout << ++count << ": [" << line << "] len=" << line.size() 
+           //<< "<br />" << endl;
+    }
+     cout << "</table>";
 
 }
